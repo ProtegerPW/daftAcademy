28f118954eee8dfe1d091df4e75b392ff9d9bd34
ProtegerPW <kobolte@gmail.com>
2021-04-14 18:10:06 +0200

    Add name

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.counter = 0
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+def hello_name_view(name: str):
+    return f"Hello {name}"
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from main import app
+import pytest
 
 client = TestClient(app)
 
@@ -9,3 +10,22 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+    
+
+
+# def test_hello_name(name):
+#     name = 'Kamila'
+#     response = client.get(f"/hello/{name}")
+#     assert response.status_code == 200
+#     assert response.text == f'"Hello {name}"'
+
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
